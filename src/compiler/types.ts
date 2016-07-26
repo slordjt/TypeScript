b85bbf8937cca7123a68c5710f5c503040497d49
Wesley Wigham <t-weswig@microsoft.com>
2016-07-25 20:00:05 -0700

    Enhance doc comments

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -1862,19 +1862,52 @@ namespace ts {
         isOptionalParameter(node: ParameterDeclaration): boolean;
 
         /**
-         * Two types are identical iff they are references to the exact same fully qualified, resolved type (usually an alias to ===)
+         * Two types are considered identical when
+         *  - they are both the `any` type,
+         *  - they are the same primitive type,
+         *  - they are the same type parameter,
+         *  - they are union types with identical sets of constituent types, or
+         *  - they are intersection types with identical sets of constituent types, or
+         *  - they are object types with identical sets of members.
+         *
+         * This relationship is bidirectional.
+         * See [here](https://github.com/Microsoft/TypeScript/blob/master/doc/spec.md#3.11.2) for more information.
          */
         isIdenticalTo(a: Type, b: Type): boolean;
         /**
-         * A type is a subtype of another if the collection of its members is contained within the collection of the other's members
+         * `a` is a ___subtype___ of `b` (and `b` is a ___supertype___ of `a`) if `a` has no excess properties with respect to `b`,
+         *  and one of the following is true:
+         *  - `a` and `b` are identical types.
+         *  - `b` is the `any` type.
+         *  - `a` is the `undefined` type.
+         *  - `a` is the `null` type and `b` is _not_ the `undefined` type.
+         *  - `a` is an enum type and `b` is the primitive type `number`.
+         *  - `a` is a string literal type and `b` is the primitive type `string`.
+         *  - `a` is a union type and each constituient type of `b` is a subtype of `b`.
+         *  - `a` is an intersection type and at least one constituent type of `a` is a subtype of `b`.
+         *  - `b` is a union type and `a` is a subtype of at least one constituent type of `b`.
+         *  - `b` is an intersection type and `a` is a subtype of each constituent type of `b`.
+         *  - `a` is a type parameter and the constraint of `a` is a subtype of `b`.
+         *  - `a` has a subset of the structural members of `b`.
+         *
+         * This relationship is directional.
+         * See [here](https://github.com/Microsoft/TypeScript/blob/master/doc/spec.md#3.11.3) for more information.
          */
         isSubtypeOf(a: Type, b: Type): boolean;
         /**
-         * Same as subtype, but all types are assignable to `any` and `any` is assignable to all types
+         * The assignable relationship differs only from the subtype relationship in that:
+         *  - the `any` type is assignable to, but not a subtype of, all types
+         *  - the primitive type `number` is assignable to, but not a subtype of, all enum types, and
+         *  - an object type without a particular property is assignable to an object type in which that property is optional.
+         *
+         * This relationship is directional.
+         * See [here](https://github.com/Microsoft/TypeScript/blob/master/doc/spec.md#3.11.4) for more information.
          */
         isAssignableTo(a: Type, b: Type): boolean;
         /**
-         * Same as assignable, but all unions with overlapping constituient types are comparable, and units of the same type are comparable
+         * True if `a` is assignable to `b`, or `b` is assignable to `a`. Additionally, all unions with
+         * overlapping constituient types are comparable, and unit types in the same domain are comparable.
+         * This relationship is bidirectional.
          */
         isComparableTo(a: Type, b: Type): boolean;
         /**
@@ -1884,14 +1917,11 @@ namespace ts {
 
         /**
          * Looks up a global symbol named "name" with meaning SymbolFlags.Type
-         *  This is identical to the internal method getGlobalType but without
-         *  the expected arity based return value.
          *  Returns the unknown type on failure.
          */
         lookupGlobalType(name: string): Type;
         /**
          * Resolve a symbol lexically at the position specified with meaning SymbolFlags.Type
-         *  This is works similarly to the internal method resolveName, but with less exposed configuration.
          *  Returns the unknown type on failure.
          */
         lookupTypeAt(name: string, position: Node): Type;
